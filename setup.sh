236ddf82898c39506df03c0cd4627d1ef8542a38
Govindarajan V <govindarajanv@outlook.com>
2021-04-12 15:56:49 +0530

    Update and rename install.sh to setup.sh

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+setup_git() {
+echo "setting up git"
+echo "Enter your name:"
+read name
+echo "Enter your email:"
+read email
+
+git config --global user.name $name
+git config --global user.email $email
+git config --global init.defaultBranch main
+git config --global credential.helper 'cache --timeout=3600'
+
+}
+
+
+setup_git()
